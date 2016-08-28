27fd0dabf63aeb9e916360cbfd0df6087ba8034c
Sharif Olorin <sio@tesser.org>
2016-08-28 02:15:55 +0000

    Add parser: assignment operators

--- l1c/src/Jouet/L1/Parser.hs
+++ l1c/src/Jouet/L1/Parser.hs
@@ -3,12 +3,23 @@
 
 module Jouet.L1.Parser (
     binOpP
+  , assnOpP
   ) where
 
 import           Data.Attoparsec.ByteString
 import           Data.Attoparsec.ByteString.Char8 (char8)
 
 import           Jouet.L1.Grammar
+
+assnOpP :: Parser AssnOp
+assnOpP = choice [
+    string "+=" *> pure OpAddEq
+  , string "-=" *> pure OpSubEq
+  , string "*=" *> pure OpMulEq
+  , string "/=" *> pure OpDivEq
+  , string "%=" *> pure OpModEq
+  , char8  '='  *> pure OpEq
+  ]
 
 binOpP :: Parser BinOp
 binOpP = choice [
