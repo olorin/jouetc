6a79f0beb61de04a199302fd5c30bebc847f0d91
Sharif Olorin <sio@tesser.org>
2016-09-04 09:26:13 +0000

    Add render utilities for the grammar

---
+++ l1c/src/Jouet/L1/Render.hs
@@ -0,0 +1,73 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -funbox-strict-fields #-}
+
+{- Render bits of the grammar. -}
+module Jouet.L1.Render (
+    renderProgram
+  , renderStmt
+  , renderExpr
+  , renderDecl
+  , renderAssn
+  , renderIdent
+  , renderAssnOp
+  , renderBinOp
+  ) where
+
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BSC
+import           Data.Monoid ((<>))
+
+import           Jouet.L1.Grammar
+
+renderProgram :: Program -> ByteString
+renderProgram (Program ss) = BS.intercalate "\n" $ renderStmt <$> ss
+
+renderStmt :: Stmt -> ByteString
+renderStmt (DeclS s) = renderDecl s <> ";"
+renderStmt (AssnS s) = renderAssn s <> ";"
+renderStmt (ReturnS s) = "return " <> renderExpr s <> ";"
+
+renderDecl :: Decl -> ByteString
+renderDecl (IntDecl i Nothing) = "int " <> renderIdent i
+renderDecl (IntDecl i (Just e)) = BS.intercalate " " [
+    "int"
+  , renderIdent i
+  , "="
+  , renderExpr e
+  ]
+
+renderAssn :: Assn -> ByteString
+renderAssn (Assn i o expr) = BS.intercalate " " [
+    renderIdent i
+  , renderAssnOp o
+  , renderExpr expr
+  ]
+
+renderExpr :: Expr -> ByteString
+renderExpr (IntE n) = BSC.pack $ show n
+renderExpr (IdentE i) = renderIdent i
+renderExpr (BinE e1 o e2) = BS.intercalate " " [
+    renderExpr e1
+  , renderBinOp o
+  , renderExpr e2
+  ]
+renderExpr (NegE expr) = "-" <> renderExpr expr
+
+renderIdent :: Ident -> ByteString
+renderIdent (Ident x) = x
+
+renderAssnOp :: AssnOp -> ByteString
+renderAssnOp OpEq = "="
+renderAssnOp OpAddEq = "+="
+renderAssnOp OpSubEq = "-="
+renderAssnOp OpMulEq = "*="
+renderAssnOp OpDivEq = "/="
+renderAssnOp OpModEq = "%="
+
+renderBinOp :: BinOp -> ByteString
+renderBinOp OpAdd = "+"
+renderBinOp OpSub = "-"
+renderBinOp OpMul = "*"
+renderBinOp OpDiv = "/"
+renderBinOp OpMod = "%"
