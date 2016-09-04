ad0c880339956bf0d05b204664ec7af44c91cdc9
Sharif Olorin <sio@tesser.org>
2016-09-04 03:42:06 +0000

    Add parser: ident

--- l1c/src/Jouet/L1/Parser.hs
+++ l1c/src/Jouet/L1/Parser.hs
@@ -4,15 +4,48 @@
 module Jouet.L1.Parser (
     binOpP
   , assnOpP
+  , identP
   ) where
 
-import           Data.Attoparsec.ByteString
+import           Data.Attoparsec.ByteString (Parser, string)
+import qualified Data.Attoparsec.ByteString as AB
 import           Data.Attoparsec.ByteString.Char8 (char8)
+
+import qualified Data.ByteString as BS
+
+import           Data.Word (Word8)
 
 import           Jouet.L1.Grammar
 
+identP :: Parser Ident
+identP = Ident <$> (BS.cons <$> AB.satisfy identHead <*> AB.takeWhile identTail)
+
+identTail :: Word8 -> Bool
+identTail 0x5f = True -- underscore
+identTail w = or [
+    isAlphaLower w
+  , isAlphaUpper w
+  , isDigit w
+  ]
+
+identHead :: Word8 -> Bool
+identHead 0x5f = True -- underscore
+identHead w = or [
+    isAlphaLower w
+  , isAlphaUpper w
+  ]
+
+isDigit :: Word8 -> Bool
+isDigit w = w >= 0x30 && w <= 0x39
+
+isAlphaUpper :: Word8 -> Bool
+isAlphaUpper w = w >= 0x41 && w <= 0x5a
+
+isAlphaLower :: Word8 -> Bool
+isAlphaLower w = w >= 0x61 && w <= 0x7a
+
 assnOpP :: Parser AssnOp
-assnOpP = choice [
+assnOpP = AB.choice [
     string "+=" *> pure OpAddEq
   , string "-=" *> pure OpSubEq
   , string "*=" *> pure OpMulEq
@@ -22,7 +55,7 @@ assnOpP = choice [
   ]
 
 binOpP :: Parser BinOp
-binOpP = choice [
+binOpP = AB.choice [
     char8 '+' *> pure OpAdd
   , char8 '-' *> pure OpSub
   , char8 '*' *> pure OpMul
